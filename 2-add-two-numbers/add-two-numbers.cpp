e06be16ec3bc7b518f74e9caea61f9b31a3bd891
Amir rashid <amirrashid9149@gmail.com>
2026-09-23 14:28:25 +0530

    Time: 4 ms (20.97%) | Memory: 78.1 MB (12.71%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -0,0 +1,105 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+
+        vector<int> n1,n2;
+       
+      ListNode* p1 = l1;
+      ListNode* p2 = l2;
+
+      while(p1)
+      {
+        n1.push_back(p1->val);
+        p1=p1->next;
+      }
+      while(p2)
+      {
+        n2.push_back(p2->val);
+        p2=p2->next;
+      }
+
+      ListNode* fn = new ListNode(-1);
+      ListNode* temp = fn;
+      int i = 0,j = 0,carry = 0;
+
+      while(i<n1.size() && j<n2.size())
+      {
+        int digit = n1[i++]+n2[j++]+carry;
+        
+        if(digit<10)
+        {
+            ListNode* nn = new ListNode(digit);
+            carry = 0;
+            temp->next = nn;
+            temp = temp->next;
+        }
+        else if(digit>9)
+        {
+            ListNode* nn = new ListNode(digit%10);
+            digit /= 10;
+            carry = digit;
+            temp->next = nn;
+            temp = temp->next;
+        }
+      }
+      while(i<n1.size()) 
+      {
+        int digit = n1[i++]+carry;
+        if(digit<10)
+        {
+            ListNode* nn = new ListNode(digit);
+            carry = 0;
+            temp->next = nn;
+            temp = temp->next;
+        }
+        else if(digit>9)
+        {
+            ListNode* nn = new ListNode(digit%10);
+            digit /= 10;
+            carry = digit;
+            temp->next = nn;
+            temp = temp->next;
+        }
+      }
+      while(j<n2.size()) 
+      {
+        int digit = n2[j++]+carry;
+        if(digit<10)
+        {
+            ListNode* nn = new ListNode(digit);
+            carry = 0;
+            temp->next = nn;
+            temp = temp->next;
+        }
+        else if(digit>9)
+        {
+            ListNode* nn = new ListNode(digit%10);
+            digit /= 10;
+            carry = digit;
+            temp->next = nn;
+            temp = temp->next;
+        }
+      }
+
+      if(carry)
+      {
+            ListNode* nn = new ListNode(carry);
+            temp->next = nn;
+            temp = temp->next;
+      }
+
+      return fn->next;
+
+    }
+
+};
