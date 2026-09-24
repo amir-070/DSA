b893e7e28bb978f3d9a3a940b6a04179e5a2c922
Amir rashid <amirrashid9149@gmail.com>
2026-09-24 16:28:45 +0530

    Time: 18 ms (74.11%) | Memory: 23.9 MB (99.06%) - LeetSync

--- 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
+++ 2042-maximum-product-difference-between-two-pairs/maximum-product-difference-between-two-pairs.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int maxProductDifference(vector<int>& nums) {
-
-        int max = 0, secmax = 0, min = 10001, secmin = 10001,n=nums.size();
-
-        for (int i = 0; i < n; i++) {
-            if (nums[i] < min) {
-                secmin = min;
-                min = nums[i];
-            } else if (nums[i] < secmin) {
-                secmin = nums[i];
+        int firstMax = 0;
+        int secondMax = 0;
+        int firstMin = 10001;
+        int secondMin = 10001;
+        int n = nums.size();
+        for(int i = 0;i<n;i++){
+            if(nums[i] > firstMax){
+                secondMax = firstMax;
+                firstMax = nums[i];
+            }
+            else if(nums[i] > secondMax){
+                secondMax = nums[i];
             }
 
-            if (nums[i] > max) {
-                secmax = max;
-                max = nums[i];
-            } else if (nums[i] > secmax) {
-                secmax = nums[i];
+            if(nums[i] < firstMin){
+                secondMin = firstMin;
+                firstMin = nums[i];
+            }
+            else if(nums[i] < secondMin){
+                secondMin = nums[i];
             }
         }
 
-        return (max * secmax - min * secmin);
+        return (firstMax*secondMax - firstMin*secondMin);
     }
 };
