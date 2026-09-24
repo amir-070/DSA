66a5e8230775ede42bcbd57b2b708ac59e758d0c
Amir rashid <amirrashid9149@gmail.com>
2026-09-24 14:52:56 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (31.59%) - LeetSync

--- 2756-buy-two-chocolates/buy-two-chocolates.cpp
+++ 2756-buy-two-chocolates/buy-two-chocolates.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
 
+        int min = INT_MAX, secmin = INT_MAX;
 
-        int min = INT_MAX,secmin = INT_MAX;
-
-
-        for(auto x :prices)
-        {
-            if(x<min)
-            {
+        for (auto x : prices) {
+            if (x < min) {
                 secmin = min;
-                min=x;
-            }
-            else if(x < secmin )
-            {
+                min = x;
+            } else if (x < secmin) {
                 secmin = x;
             }
         }
 
-        int val = money - min - secmin;
-
-        if(val < 0) return money;
-        else return val;
+        if ((money - min - secmin) < 0)
+            return money;
+        else
+            return money - min - secmin;
     }
 };
