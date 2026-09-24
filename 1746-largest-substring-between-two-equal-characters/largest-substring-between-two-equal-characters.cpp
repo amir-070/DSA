593b66b6842aca09c86286b24cf318e73d779ea6
Amir rashid <amirrashid9149@gmail.com>
2026-09-24 15:13:46 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (22.79%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -2,18 +2,30 @@ class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
 
-        int str = -1;
+        // int str = -1;
 
-        for(int i = 0 ;i<s.size()-1;i++)
+        // for(int i = 0 ;i<s.size()-1;i++)
+        // {
+        //     for(int j = i+1;j<s.size();j++)
+        //     {
+        //         if(s[i] == s[j])
+        //         {
+        //             str = max(str,j-i-1);
+        //         }
+        //     }
+        // }
+        // return str;
+
+
+        vector<int> pos(26,-1);
+
+        int m_len = -1;
+        for(int i=0;i<s.size();i++)
         {
-            for(int j = i+1;j<s.size();j++)
-            {
-                if(s[i] == s[j])
-                {
-                    str = max(str,j-i-1);
-                }
-            }
+            if(pos[s[i] - 'a'] == -1) pos[s[i] - 'a'] = i;
+            else m_len = max(m_len,i-pos[s[i] - 'a']-1);
         }
-        return str;
+
+        return m_len;
     }
 };
