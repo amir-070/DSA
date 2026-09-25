ae9c9d4b9f0034929d44a9933c4f37c730e94679
Amir rashid <amirrashid9149@gmail.com>
2026-09-25 10:50:20 +0530

    Time: 7 ms (56.90%) | Memory: 15.1 MB (70.76%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     bool repeatedSubstringPattern(string s) {
-        int n = s.length();
-        for (int i = n / 2; i >= 1; i--) {
-            if (n % i == 0) {
-                string repeated = "";
-                for (int j = 0; j < n / i; j++) {
-                    repeated += s.substr(0, i);
-                }
-                if (repeated == s) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        // int n = s.length();
+        // for (int i = n / 2; i >= 1; i--) {
+        //     if (n % i == 0) {
+        //         string repeated = "";
+        //         for (int j = 0; j < n / i; j++) {
+        //             repeated += s.substr(0, i);
+        //         }
+        //         if (repeated == s) {
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
 
-        // string concatenated = s + s;
-        // return concatenated.substr(1, concatenated.length() - 2).find(s) != string::npos;
+        string concatenated = s + s;
+        return concatenated.substr(1, concatenated.length() - 2).find(s) != string::npos;
     }
 };
