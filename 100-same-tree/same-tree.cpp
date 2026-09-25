fdcbdd76e6984e754ad8ea2630be598f35afb006
Amir rashid <amirrashid9149@gmail.com>
2026-09-25 16:37:22 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (12.96%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -23,17 +23,11 @@ public:
 
         // actual solution
 
-        if (p == NULL && q == NULL) {
-            return true;
-        }
+        if (p == NULL && q == NULL) return true;
 
-        if (p == NULL || q == NULL) {
-            return false;
-        }
+        if (p == NULL || q == NULL) return false;
 
-        if (p->val == q->val) {
-            return isSameTree(p->left, q->left) & isSameTree(p->right, q->right);
-        }
+        if (p->val == q->val) return isSameTree(p->left, q->left) & isSameTree(p->right, q->right);
 
         return false;
     }
