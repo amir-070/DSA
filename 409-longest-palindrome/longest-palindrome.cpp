6150d67abca55b0949c6c1dfdfb8725bc32fb872
Amir rashid <amirrashid9149@gmail.com>
2026-09-25 09:03:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (84.95%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        int freq[128] = {0};  int res = 0; int odd = 0;
+        int freq[123] = {0};  int res = 0; int odd = 0;
         for (char ch : s) ++freq[ch];
-        for (int i = 0; i < 128; ++i) {
+        for (int i = 0; i < 123; ++i) {
             if (freq[i] % 2 == 1) { res += freq[i] - 1; odd = 1; }
             else res += freq[i];
         }
