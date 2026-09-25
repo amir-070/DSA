d1630490bcd291d9418006c315008d9fb8c7981b
Amir rashid <amirrashid9149@gmail.com>
2026-09-25 16:29:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (46.88%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -15,7 +15,7 @@ public:
                 return false;
         }
         if (p->val != q->val) return false;
-        if (((p->left == nullptr) && (q->left != nullptr))&&((p->left != nullptr) && (q->left == nullptr))&&((p->right == nullptr) && (q->right != nullptr))&&((p->right != nullptr) && (q->right == nullptr))) return false;
+        if (((!p->left) && (q->left))&&((p->left != nullptr) && (q->left == nullptr))&&((p->right == nullptr) && (q->right != nullptr))&&((p->right != nullptr) && (q->right == nullptr))) return false;
 
 
         return isSameTree(p->left,q->left)&isSameTree(p->right,q->right);
