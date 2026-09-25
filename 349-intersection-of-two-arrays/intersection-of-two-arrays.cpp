0ed3e1cdd905269525d2b233554331c2b2ca6ac4
Amir rashid <amirrashid9149@gmail.com>
2026-09-25 17:43:44 +0530

    Time: 3 ms (38.80%) | Memory: 14.8 MB (8.11%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,21 +1,44 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-      unordered_set<int> st;
 
-      for(auto i:nums1) st.insert(i);
+        // My solution 
+    //   unordered_set<int> st;
 
-      vector<int> res;
-      
-      for(int i=0;i<nums2.size();i++)
-      {
-        if(st.find(nums2[i]) != st.end())
-        {
-            res.push_back(nums2[i]);
-            st.erase(nums2[i]);
+    //   for(auto i:nums1) st.insert(i);
+
+    //   vector<int> res;
+
+    //   for(int i=0;i<nums2.size();i++)
+    //   {
+    //     if(st.find(nums2[i]) != st.end())
+    //     {
+    //         res.push_back(nums2[i]);
+    //         st.erase(nums2[i]);
+    //     }
+    //   }
+
+    //   return res;
+
+        set<int> n1, n2;
+        int i = 0;
+        while(i < nums1.size() || i < nums2.size()){
+            if(i < nums1.size()){
+                n1.insert(nums1[i]);
+            }
+            if(i < nums2.size()){
+                n2.insert(nums2[i]);
+            }
+            i++;
         }
-      }
+        vector<int> ans;
+        for(int x : n1){
+            auto it = n2.find(x);
+            if(it != n2.end()){
+                ans.push_back(x);
+            }
+        }
+        return ans;
 
-      return res;
     }
 };
