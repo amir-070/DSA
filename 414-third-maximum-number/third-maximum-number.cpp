5c14f7fd61dac2c4821662de5d3ffa75e02a0774
Amir rashid <amirrashid9149@gmail.com>
2026-09-25 09:52:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (46.89%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -5,19 +5,11 @@ public:
         long long max1 = LLONG_MIN, max2 = LLONG_MIN, max3 = LLONG_MIN;
 
         for (auto i : nums) {
-            if (i > max1) {
-                max3 = max2;
-                max2 = max1;
-                max1 = i;
-            }
+            if (i > max1) { max3 = max2; max2 = max1; max1 = i; }
+            
+            else if (i > max2 and i != max1) { max3 = max2; max2 = i; }
 
-            else if (i > max2 and i != max1) {
-                max3 = max2;
-                max2 = i;
-            }
-
-            else if (i > max3 and i != max2 and i != max1)
-                max3 = i;
+            else if (i > max3 and i != max2 and i != max1) max3 = i;
         }
 
         if (max3 == LLONG_MIN)
